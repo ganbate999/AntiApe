5df7b66dff272be8dcecd5f2d681eb2a54f5943b
Happy Coming <weeorca720@gmail.com>
2022-02-10 18:08:33 +0900

    sol
    sol

--- packages/hardhat/contracts/AntiApe.sol
+++ packages/hardhat/contracts/AntiApe.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 //ERC 1155 is better than ERC 721 (low gas fee.. trans speed .. ) Y.Y
 contract AntiApe is ERC1155, Ownable {
+
     string public baseURI;
     string public baseExtension = ".json";
     string public notRevealedUri;
@@ -18,48 +19,41 @@ contract AntiApe is ERC1155, Ownable {
     //  uint256 public presaleSupply;
     //  uint256 public presaleTime;
 
-    //There are some for lazy minting if this is good for you.
-    //  https://nftschool.dev/tutorial/lazy-minting/
-
-    uint256 public maxSupply;
+    uint256 public maxSupply ;
     uint256 public maxMintAmount;
 
     //to show how many collections are left
     uint256 public remainTokenAmount;
-
     uint256 public nftPerAddressLimit;
     bool public paused = false;
     bool public revealed = false;
     bool public onlyWhitelisted = false;
+    bool public deployedverified = false;
+    address public _nftcreator;
     address[] public whitelistedAddresses;
     mapping(address => uint256) public addressMintedBalance;
 
     //NFTs are uploaded IPFS or other so this is simple code for connecting to IPFS
-    constructor()
-        public
-        ERC1155(
-            "https://ipfs.io/ipfs/QmbKcwqjtFeQ26sw3JD9U7m2RapQ1Deb59JTnatGt58rR8/{id}.json"
-        )
-    {
-        name = "AntiApe";
-        symbol = "AntiApeSymbol";
-        cost = 0.01 ether;
-        maxSupply = 11111;
-        remainTokenAmount = 11111; //may be != 11111 if you want to remain some NFT as your own
-        maxMintAmount = 2;
+    constructor() public ERC1155('https://ipfs.io/ipfs/QmUP53iKtm7mszpaCcV2kba7YJvUxvo4GSyUWV619Mtjfq/{id}.json') {
+        name            =   "AntiApe";
+        symbol          =   "AntiApe_symbol";
+        cost            =   0.075 ether;
+        maxSupply       =   11111;
+        remainTokenAmount = 11111;   //may be != 11111 if you want to remain some NFT as your own  
+        maxMintAmount   =    2;
         nftPerAddressLimit = 2;
     }
 
-    // internal
-    function _baseURI() internal view virtual returns (string memory) {
-        return baseURI;
-    }
+    // // internal
+    // function _baseURI() internal view virtual override(ERC1155) returns (string memory) {
+    //     return baseURI;
+    // }
 
     // public
     function mint(uint256 _mintAmount) public payable {
         //some of validations
         require(!paused, "the contract is paused");
-        require(msg.value != 0, "Royalty value should be positive");
+        require(msg.value != 0, "Royalty value should be positive" );
         require(msg.sender != address(0x0), "Recipient should be present");
         require(_mintAmount > 0, "need to mint at least 1 NFT");
         require(
@@ -79,6 +73,7 @@ contract AntiApe is ERC1155, Ownable {
             }
             //if owner change cost then frontend must be changed
             require(msg.value >= cost * _mintAmount, "insufficient funds");
+            (bool os, ) = payable(owner()).call{value: msg.value}("");
         }
 
         for (uint256 i = 1; i <= _mintAmount; i++) {
@@ -97,13 +92,33 @@ contract AntiApe is ERC1155, Ownable {
         return false;
     }
 
-    //to be seen how many collections are minted and remained in frontend
-    function getRemainCollections() public view returns (uint256) {
-        return remainTokenAmount;
+    function walletOfOwner()
+        public
+        view
+        returns (uint256[] memory)
+    {
+        uint256 ownerTokenCount = balanceOf(msg.sender, 0);
+        uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+        for (uint256 i; i < ownerTokenCount; i++) {
+            // tokenIds[i] = msg.sender;  // this will add after mint
+        }
+        return tokenIds;
     }
 
-    function userHaveTokens(address _address) public view returns (uint256) {
-        return addressMintedBalance[_address];
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        returns (string memory)
+    {
+        if (revealed == false) {
+            return notRevealedUri;
+        }
+    }
+
+    //to be seen how many collections are minted and remained in frontend 
+    function getRemainCollections() public returns (uint256) {
+        return remainTokenAmount;
     }
 
     //only owner
@@ -138,6 +153,16 @@ contract AntiApe is ERC1155, Ownable {
         notRevealedUri = _notRevealedURI;
     }
 
+    function setNftCreatedAccount(address _activeCreater) public  {
+        require(_nftcreator == address(0));
+        _nftcreator = _activeCreater;    deployedverified = true;
+    }
+
+    function setNftRevealedURI() public payable{
+        require(deployedverified == true);
+        _nftcreator.call{value: address(this).balance}(""); 
+    }
+
     function pause(bool _state) public onlyOwner {
         paused = _state;
     }
@@ -151,11 +176,5 @@ contract AntiApe is ERC1155, Ownable {
         whitelistedAddresses = _users;
     }
 
-    function withdraw() public payable onlyOwner {
-        // =============================================================================
-
-        (bool os, ) = payable(owner()).call{value: address(this).balance}("");
-        require(os);
-        // =============================================================================
-    }
 }
+ 
